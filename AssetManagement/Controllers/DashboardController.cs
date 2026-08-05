827f4a6030bbdb3a74ab8d68ae2182897a1a5e62
xosc <urlocaldumbass@proton.me>
2026-08-05 23:35:05 +1200

    dashboard

---
+++ AssetManagement/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using AssetManagement.Data;
+using AssetManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetManagement.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly AssetManagementContext _context;
+        public DashboardController(AssetManagementContext context)
+        {
+            _context = context;
+        }
+
+        public class DashboardViewModel
+        {
+            public int TotalAssets { get; set; }
+
+            public int AvailableAssets { get; set; }
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new DashboardViewModel
+            {
+                TotalAssets = await _context.Asset.CountAsync(),
+                AvailableAssets = await _context.Asset.CountAsync(a => a.Status == AssetStatus.Available),
+            };
+
+            return View(model);
+        }
+    }
+}
